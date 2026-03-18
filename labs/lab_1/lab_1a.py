8ccfb76bcee051c4c69d686725b616ca72e1234e
hubowen09-hub <hubowen09@gmail.com>
2026-03-18 11:36:17 -0500

    Add name and intro
    I added name and intro

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Belinda Hu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is Bowen Hu, and I go by Belinda Hu. I'm a Sophomore from Illinois. My hobbies are singing and listening to music! My favorite subjects at school are math and choir. My favorite flavor of ice cream is coffee.")
 
 if __name__ == "__main__":
     main()
