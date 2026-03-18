facddf0bd12e5b43eee9291ed7ef7af1d3b1cdd6
hubowen09-hub <hubowen09@gmail.com>
2026-03-18 12:12:47 -0500

    Update lab_1a.py changed comments
    I add comments for the robots speed.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -3,6 +3,7 @@ lab_1a.py
 
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
+This is to simulate a change made on a robot: robot_speed = 5 # m/s
 """
 
 def main():
@@ -12,6 +13,5 @@ def main():
 
     print(f"{name}, Welcome to the CSS course!")
     print("Hi everyone! My name is Bowen Hu, and I go by Belinda Hu. I'm a Sophomore from Illinois. My hobbies are singing and listening to music! My favorite subjects at school are math and choir. My favorite flavor of ice cream is coffee.")
-
 if __name__ == "__main__":
     main()
